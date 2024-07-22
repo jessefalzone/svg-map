39c823f35274088dcd5989318faba70d1ff04d02
Jesse Falzone <allow.hams-06@icloud.com>
2024-07-22 19:55:48 -0400

    Refactor with bs4.

--- map-to-svg.py
+++ map-to-svg.py
@@ -2,19 +2,18 @@
 
 import argparse
 import sys
-from bs4 import BeautifulSoup, SoupStrainer
+from bs4 import BeautifulSoup, SoupStrainer, Tag
 
 
-def convert_map_to_svg(file_path: str) -> str:
+def main(file_path: str) -> Tag:
     """Convert image map areas to SVG shapes."""
+
     try:
         if not file_path.endswith((".map", ".html")):
             print("File should be .map or .html.")
             sys.exit(1)
         with open(file_path, "r") as file:
-            soup = BeautifulSoup(
-                file, "html.parser", parse_only=SoupStrainer(["img", "map"])
-            )
+            soup = BeautifulSoup(file, "lxml", parse_only=SoupStrainer(["img", "map"]))
         return generate_svg(soup)
     except FileNotFoundError:
         print(f"File not found: {file_path}")
@@ -26,6 +25,7 @@ def convert_map_to_svg(file_path: str) -> str:
 
 def get_svg_shape(area) -> str:
     """Given an <area> shape, get the equivalent SVG shape."""
+
     map_shape_to_svg = {
         "poly": "polygon",
         "rect": "rect",
@@ -34,12 +34,21 @@ def get_svg_shape(area) -> str:
     return map_shape_to_svg[area]
 
 
-def get_coordinate_attrs(points: str, shape: str) -> str:
+def set_coordinate_attrs(element: Tag, points: str, shape: str):
     """Convert <area> coordinates to SVG coordinates."""
+
+    if not points:
+        print("Coordinates missing from element: ", element)
+        sys.exit(1)
+
     if shape == "circle":
-        point_list = points.split(",")
         # <area> coords format is x,y,radius.
-        return 'cx="{}" cy="{}" r="{}"'.format(*point_list)
+        point_list = points.split(",")
+        element.attrs = element.attrs | {
+            "cx": point_list[0],
+            "cy": point_list[1],
+            "r": point_list[2],
+        }
 
     elif shape == "rect":
         # <area> coordinate format is x1,y1,x2,y2, coordinates for top-left and
@@ -49,7 +58,6 @@ def get_coordinate_attrs(points: str, shape: str) -> str:
         # Get the width and height based on the two X coordinates. Use the
         # absolute value because some image map generators seem to incorrectly
         # put the bottom-right coordinate first.
-
         # Width is x2 - x1.
         width = abs(coords[2] - coords[0])
 
@@ -57,22 +65,59 @@ def get_coordinate_attrs(points: str, shape: str) -> str:
         height = abs(coords[3] - coords[1])
 
         # (x,y) is the coordinate of the top-left corner. Use the minimum value
-        # in case the image map generator put the bottom-right corner first.
+        # (leftmost and topmost) in case the image map generator put the
+        # bottom-right corner first.
         x = min(coords[0], coords[2])
         y = min(coords[1], coords[3])
 
         # rect's x and y attributes are the coordinates of the top left corner.
-        attrs = f'x="{x}" y="{y}" width="{width}" height="{height}"'
-        return attrs
+        element.attrs = element.attrs | {
+            "x": x,
+            "y": y,
+            "width": width,
+            "height": height,
+        }
 
     else:
-        # The shape is a polygon.
-        return f'points="{points}"'
+        # The shape is a polygon. We can use the points directly.
+        element["points"] = points
 
 
-def generate_svg(soup) -> str:
+def build_fill_element(coords: str, shape: str) -> Tag:
+    """Create an element for the shape fill."""
+
+    fill = BeautifulSoup("lxml").new_tag(shape)
+    fill["filter"] = "url(#blur)"
+    fill["class"] = "fill"
+    set_coordinate_attrs(fill, coords, shape)
+    return fill
+
+
+def build_stroke_element(coords: str, shape: str) -> Tag:
+    """Create an element for the shape stroke."""
+
+    classes = ["stroke"]
+    if args.visible_strokes:
+        classes.append("stroke--visible")
+
+    stroke = BeautifulSoup("lxml").new_tag(
+        shape,
+        attrs={
+            "class": classes,
+            "stroke-linejoin": "round",
+        },
+    )
+    set_coordinate_attrs(stroke, coords, shape)
+    return stroke
+
+
+def generate_svg(soup) -> Tag:
     """Build the SVG string."""
+
+    # TODO: remove inline style.
     soup.img["style"] = "max-width:100%;height:auto"
+
+    # There's no image map anymore!
     del soup.img["usemap"]
 
     img_width = soup.img.get("width")
@@ -85,19 +130,29 @@ def generate_svg(soup) -> str:
         print(f"Image:\n\t{soup.img}")
         sys.exit(1)
 
-    svg_string = f"""
-<div style="position:relative;display:inline-block">
-    {soup.img}
-    <svg
-        viewBox="0 0 {img_width} {img_height}"
-        xmlns="http://www.w3.org/2000/svg"
-        version="1.1"
-        style="position:absolute;top:0;left:0"
-    >
-    """
-    svg_string = (
-        svg_string
-        + """
+    # Create a div that will contain the original image and the SVG.
+    wrapper = soup.new_tag("div")
+    # TODO: remove inline style.
+    wrapper["style"] = "position:relative;display:inline-block"
+    wrapper.append(soup.img)
+
+    # Now create the svg tag.
+    svg = soup.new_tag("svg")
+    svg.attrs = {
+        # The viewbox needs to be the same dimensions as the image
+        # in order to be responsive.
+        "viewBox": f"0 0 {img_width} {img_height}",
+        "xmlns": "http://www.w3.org/2000/svg",
+        "version": "1.1",
+        # This overlays the svg on the image.
+        "style": "position:absolute;top:0;left:0",
+    }
+    wrapper.append(svg)
+
+    # Insert some styles and filters.
+    svg.append(
+        BeautifulSoup(
+            """
     <defs>
         <style type="text/css"><![CDATA[
             .fill {
@@ -134,41 +189,48 @@ def generate_svg(soup) -> str:
     </defs>
     <filter id="blur">
         <feGaussianBlur in="SourceGraphic" stdDeviation="4" />
-    </filter>
-    """
+    </filter>""",
+            "lxml",
+        )
     )
 
-    # Generate regions.
+    # Generate the regions from the image map areas.
     areas = soup.map.findAll("area")
+
     for area in areas:
+        # The parent element may be either the svg or an <a> tag.
+        parent = svg
         shape = get_svg_shape(area.get("shape"))
-        points = get_coordinate_attrs(area.get("coords"), shape)
+        href = area.get("href")
+
+        if href:
+            # Hyperlink the shape.
+            link = soup.new_tag(
+                "a",
+                attrs={
+                    "href": href,
+                    "alt": area.get("alt"),
+                    "target": area.get("target", "_self"),
+                },
+            )
+            svg.append(link)
+
+            # Make this link the parent element so it will wrap our shapes.
+            parent = link
 
         # `fill` is the inner region of the shape.
-        region_string = f'<{shape} {points} filter="url(#blur)" class="fill" />'
+        fill_element = build_fill_element(area.get("coords"), shape)
+        parent.append(fill_element)
+
+        if args.no_strokes:
+            continue
 
         # `stroke` is the outline of the shape. This is separate to maintain
         # a sharp stroke while blurring the fill.
-        if not args.no_strokes:
-            classes = "stroke"
-            if args.visible_strokes:
-                classes = classes + " stroke--visible"
-            stroke_string = (
-                f'<{shape} {points} class="{classes}" stroke-linejoin="round" />'
-            )
-            region_string = f"{region_string}{stroke_string}"
+        stroke_element = build_stroke_element(area.get("coords"), shape)
+        parent.append(stroke_element)
 
-        href = area.get("href")
-        if href:
-            # Hyperlink the shape.
-            alt_text = area.get("alt")
-            target = area.get("target", "_self")
-            region_string = f'<a href="{href}" alt="{alt_text}" target="{target}">{region_string}</a>'
-
-        svg_string = svg_string + region_string
-
-    # Close out the open elements.
-    return svg_string + "</svg></div>"
+    return wrapper
 
 
 if __name__ == "__main__":
@@ -190,6 +252,6 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    svg = convert_map_to_svg(args.file)
+    svg = main(args.file)
     print("\n")
-    print(BeautifulSoup(svg, "html.parser").prettify(formatter="minimal"))
+    print(svg.prettify(formatter="minimal"))
